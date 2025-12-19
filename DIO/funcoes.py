9118f3a6bae6e80f1f2b5f477a151368302d775b
aabdalaa <andreluizabdalacosta@gmail.com>
2025-12-18 22:03:58 -0300

    versão 1 - app bancario

--- DIO/funcoes.py
+++ DIO/funcoes.py
@@ -1,8 +1,76 @@
-def exibir_mensagem():
-    print("Olá mundo!")
+saldo = 0
+limite = 1000
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 3
 
-def exibir_mensagem2(nome):
-    print(f"Seja bem-vindo, {nome}!")
 
-exibir_mensagem2(nome = str(input("Qual seu nome ? ")))
-"""teste"""
+def depositar(valor):
+            global saldo, extrato
+            if valor > 0:
+                saldo += valor
+                extrato += f"Depósito: R$ {valor:.2f}\n"
+
+            else:
+                print("Operação falhou! O valor informado é inválido.")
+
+def sacar(valor):
+            global saldo, extrato, numero_saques
+            excedeu_saldo = valor > saldo
+
+            excedeu_limite = valor > limite
+
+            excedeu_saques = numero_saques >= LIMITE_SAQUES
+
+            if excedeu_saldo:
+                print("Operação falhou! Você não tem saldo suficiente.")
+
+            elif excedeu_limite:
+                print("Operação falhou! O valor do saque excede o limite.")
+
+            elif excedeu_saques:
+                print("Operação falhou! Número máximo de saques excedido.")
+
+            elif valor > 0:
+                saldo -= valor
+                extrato += f"Saque: R$ {valor:.2f}\n"
+                numero_saques += 1
+
+            else:
+                print("Operação falhou! O valor informado é inválido.")
+
+def exibir_extrato(saldo):    
+            print("\n================ EXTRATO ================")
+            print("Não foram realizadas movimentações." if not extrato else extrato)
+            print(f"\nSaldo: R$ {saldo:.2f}")
+            print("==========================================")
+
+menu = """
+
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[q] Sair
+
+=> """
+
+while True:
+
+    opcao = input(menu)
+
+    if opcao == "d":
+        valor = float(input("Informe o valor do depósito: "))
+        depositar(valor)       
+
+    elif opcao == "s":
+        valor = float(input("Informe o valor do saque: "))
+        sacar(valor)
+
+    elif opcao == "e":
+        exibir_extrato(saldo)
+
+    elif opcao == "q":
+        break
+
+    else:
+        print("Operação inválida, por favor selecione novamente a operação desejada.")
